dd63137d56cb72d1e05555e61c3e56b802376187
Asirwad <asirwadsali@gmail.com>
2025-03-12 16:00:20 +0530

    publisher added

---
+++ producer/src/main/java/com/asir/wikimedia/producer/stream/WikiMediaStreamConsumer.java
@@ -0,0 +1,29 @@
+package com.asir.wikimedia.producer.stream;
+
+import com.asir.wikimedia.producer.producer.WikiMediaProducer;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class WikiMediaStreamConsumer {
+    private final WebClient webClient;
+    private final WikiMediaProducer producer;
+
+    public WikiMediaStreamConsumer(
+            WebClient.Builder webClientBuilder,
+            WikiMediaProducer producer
+    ) {
+        this.webClient = webClientBuilder
+                .baseUrl("https://stream.wikimedia.org/v2")
+                .build();
+        this.producer = producer;
+    }
+
+    public void consumeStreamAndPublish() {
+        webClient.get()
+                .uri("/stream/recentchange")
+                .retrieve()
+                .bodyToFlux(String.class)
+                .subscribe(producer::sendMessage);
+    }
+}
